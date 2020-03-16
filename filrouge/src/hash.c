006d086d29327e960c7e587a544d2d86c31e3e50
NicovincX2 <nicolas.vincent100@gmail.com>
2020-03-16 18:59:12 +0100

    Une string plus adaptée

--- filrouge/src/hash.c
+++ filrouge/src/hash.c
@@ -15,10 +15,10 @@ unsigned long hash(const unsigned char *str)
 #ifdef UNIT_TEST
 int main(void) {
     unsigned long h;
-    const unsigned char *test = (const unsigned char *)"test";
+    const unsigned char *test = (const unsigned char *)"Le module hash devra contenir l'implémentation de cette fonction de hachage. Bien qu'elle ne soit utilisée que par le module directory, on décide ici de placer cette fonction dans un module à part, ce qui permettrait d'étendre votre projet en ajoutant d'autres fonctions de hachage pour évaluer leur impact sur la façon dont se remplit votre table de hachage. Les fonctions de hachage sont aussi indépendantes du reste du projet, et peuvent de fait être testées de manière autonome.";
     
     h = hash(test);
-    printf("Hash de 'test': '%li'\n", h);
+    printf("Hash de notre string: '%li'\n", h);
     
     return EXIT_SUCCESS;
 }
