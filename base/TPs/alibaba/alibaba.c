9925ef5f67e23df695b0d3b5c94a8aaefe44975a
NicovincX2 <nicolas.vincent100@gmail.com>
2020-03-16 14:49:02 +0100

    Ajout Makefile avec test conditionnel Suppression des fichiers .o Correction de ldc_liberee (liste circulaire attention) Utilisation de pointeurs de pointeurs Correction de l'initialisation

--- base/TPs/alibaba/alibaba.c
+++ base/TPs/alibaba/alibaba.c
@@ -11,8 +11,7 @@
 
 struct _cellule {
     uint32_t val;
-    struct _cellule *suiv;
-    struct _cellule *prec;
+    struct _cellule *suiv, *prec;
 };
 
 /*
@@ -21,40 +20,32 @@ struct _cellule {
     Ceci aura pour avantage de n'avoir qu'une seule variable à traiter par liste.
 */
 struct _ldc {
+    cellule *tete, *queue;
     uint32_t taille;
-    cellule *tete;
-    cellule *queue;
 };
 
-// initialiser les pointeurs d'entrée à NULL, obligatoire avant toute utilisation de la liste
-void ldc_init(ldc *l) {
-    l->tete = NULL;
-    l->queue = NULL;
-    l->taille = 0;
-}
-
 // insere la valeur n en fin (ou début car ciruclaire) de la liste pointee par l
-void ldc_insere_fin(ldc *l, uint32_t n) {
+void ldc_insere_fin(ldc **pl, uint32_t n) {
     cellule *queue = (cellule *)malloc(sizeof(cellule));
     assert(queue != NULL);
     queue->val = n;
-    if (l->queue) {
-        queue->suiv = l->tete;
-        queue->prec = l->queue;
-        l->queue->suiv = queue;
+    if ((*pl)->queue) {
+        queue->suiv = (*pl)->tete;
+        queue->prec = (*pl)->queue;
+        (*pl)->queue->suiv = queue;
     } else {
         queue->suiv = queue;
         queue->prec = queue;
-        l->tete = queue;
+        (*pl)->tete = queue;
     }
-    l->queue = queue;
-    ++(l->taille);
+    (*pl)->queue = queue;
+    ++((*pl)->taille);
 }
 
 // affiche le contenu de la liste l
-void ldc_affiche(ldc l) {
-    cellule *tete = l.tete;
-    uint32_t taille = ldc_taille(l);
+void ldc_affiche(ldc *l) {
+    cellule *tete = l->tete;
+    uint32_t taille = l->taille;
     printf("Taille : %i \n", taille);
     while (taille != 0) {
         printf("%i -> ", tete->val);
@@ -64,16 +55,11 @@ void ldc_affiche(ldc l) {
     printf("\n");
 }
 
-// retourne le nombre d'elements contenus dans l
-uint32_t ldc_taille(ldc l) {
-    return l.taille;
-};
-
 // supprime l'element e de la liste pointee par pl.
 // precondition: *pl non vide, e non null, e dans *pl
-void ldc_supprime(ldc *l, uint32_t e) {
-    uint32_t taille = ldc_taille(*l);
-    cellule *avant_recherchee = l->tete;
+void ldc_supprime(ldc **pl, uint32_t e) {
+    uint32_t taille = (*pl)->taille;
+    cellule *avant_recherchee = (*pl)->tete;
     assert(taille != 0);
 
     while (avant_recherchee->suiv != NULL && taille != 0 && avant_recherchee->suiv->val != e) {
@@ -86,39 +72,48 @@ void ldc_supprime(ldc *l, uint32_t e) {
         cellule *recherchee = avant_recherchee->suiv;
         avant_recherchee->suiv = recherchee->suiv;
         recherchee->suiv->prec = avant_recherchee;
-        if (recherchee == l->tete) {
-            l->tete = recherchee->suiv;
+        if (recherchee == (*pl)->tete) {
+            (*pl)->tete = recherchee->suiv;
         }
-        if (recherchee == l->queue) {
-            l->queue = avant_recherchee;
+        if (recherchee == (*pl)->queue) {
+            (*pl)->queue = avant_recherchee;
         }
         free(recherchee);
-        --(l->taille);
+        --((*pl)->taille);
     }
 
 }
 
-// vide la liste pointee par l, et libere la memoire.
-// En sortie, l == NULL
-void ldc_libere(ldc *l) {
-    cellule *tete = l->tete;
-    cellule_libere(tete);
-    ldc_init(l);
+// vide la liste pointee par pl, et libere la memoire.
+// En sortie, pl == NULL
+void ldc_libere(ldc **pl) {
+    cellule *current = (*pl)->tete;
+    uint32_t taille = (*pl)->taille;
+    while (taille != 0) {
+        cellule *temp = current;
+        current = current->suiv;
+        free(temp);
+        --taille;
+    }
+    *pl = NULL;
 }
 
-void cellule_libere(cellule *c) {
-    if (c != NULL) {
-        cellule_libere(c->suiv);
-        free(c);
+void ldc_init(ldc **pl) {
+    if (*pl == NULL) {
+        *pl = (ldc *)malloc(sizeof(ldc));
     }
+    (*pl)->tete = NULL;
+    (*pl)->queue = NULL;
+    (*pl)->taille = 0;
 }
 
 int main(void) {
 
 #ifdef TESTS
-    ldc liste;
-
+    printf("Entrée dans les tests \n");
+    ldc *liste = NULL;
     ldc_init(&liste);
+    
     ldc_affiche(liste);
     ldc_insere_fin(&liste, 1);
     ldc_affiche(liste);
@@ -136,27 +131,33 @@ int main(void) {
     ldc_affiche(liste);
     ldc_supprime(&liste, 2);
     ldc_affiche(liste);
+    ldc_insere_fin(&liste, 3);
+    ldc_insere_fin(&liste, 4);
+    ldc_insere_fin(&liste, 5);
     ldc_libere(&liste);
 #else
-    ldc voleurs;
+    ldc *voleurs = NULL;
     ldc_init(&voleurs);
 
     for (uint32_t i = 1; i <= 41; i++) {
         ldc_insere_fin(&voleurs, i);
     }
+    printf("Affichage des voleurs \n");
     ldc_affiche(voleurs);
 
-    ldc perdants;
+    ldc *perdants = NULL;
     ldc_init(&perdants);
     uint32_t arbitre = 3;
 
-    while (ldc_taille(voleurs) > 2) {
+    while (voleurs->taille > 2) {
         ldc_supprime(&voleurs, arbitre);
         ldc_insere_fin(&perdants, arbitre);
         arbitre += 3;
         arbitre %= 41;
     }
+    printf("Affichage des voleurs \n");
     ldc_affiche(voleurs);
+    printf("Affichage des perdants \n");
     ldc_affiche(perdants);
     ldc_libere(&voleurs);
     ldc_libere(&perdants);
